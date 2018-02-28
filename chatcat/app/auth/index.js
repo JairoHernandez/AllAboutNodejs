08e67c46a0ddf1f1d0e1e3edded673fd9cb89998
JairoHernandez <jairomh@hotmail.com>
2018-02-28 17:38:11 -0600

    Social Authentication: Database helpers

--- chatcat/app/auth/index.js
+++ chatcat/app/auth/index.js
@@ -2,6 +2,7 @@
 const passport = require('passport');
 const config = require('../config');
 const FacebookStrategy = require('passport-facebook').Strategy;
+const h = require('../helpers');
 
 module.exports = () => {
 
@@ -9,6 +10,19 @@ module.exports = () => {
         // Find a user in local mongodb using profile.id.
         // If user is found return the user data using done().
         // If user profile is not found, create one in the local db and return.
+        h.findOne(profile.id).then(result => {
+            if (result) {
+                // null posiito is where error is held if you want to check 
+                // for server errors place null in most cases
+                // done() returns result
+                done(null, result);
+            } else {
+                // Create new user and return
+                h.createNewUser(profile)
+                    .then(newChatUser => done(null, newChatUser))
+                    .catch(error => console.log('Error when creating new user.'));
+            } 
+        })
     };
 
     // accessToken & refreshToken are provided by Facebook OAuth 2.0.
--- chatcat/app/helpers/index.js
+++ chatcat/app/helpers/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const router = require('express').Router();
+const db = require('../db');
 
 // Iterate through the routes object and mount the routes.
 let _registerRoutes = (routes, method) => {
@@ -26,6 +27,34 @@ let route = routes => {
     return router;
 }
 
+// Find a single user based on a key
+let findOne = profileID => {
+    return db.userModel.findOne({
+        'profileId': profileID
+    });
+}
+
+// Create a new user and returns that instance. Use ES6 promis this time around.
+let createNewUser = profile => {
+    return new Promise ((resolve, reject) => {
+        let newchatUser = new db.userModel({
+            profileId: profile.id,
+            fullName: profile.displayName,
+            profilePic: profile.photos.value || ''
+        });
+
+        newChatUser.save(error => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(newChatUser);
+            }
+        });
+    });
+}
+
 module.exports = {
-    route
+    route,
+    findOne,
+    createNewUser
 };
